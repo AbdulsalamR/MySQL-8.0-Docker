2a174934dc302e83741c077f5711e021e5d2f8bf
ADETOLAR01 <111161031+ADETOLAR01@users.noreply.github.com>
2022-09-13 08:33:24 +0100

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}
