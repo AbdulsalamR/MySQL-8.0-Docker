d9cf64d3059574fdb0b29ebea3b8fe4a6801efd9
ADETOLAR01 <111161031+ADETOLAR01@users.noreply.github.com>
2022-09-13 09:07:48 +0100

    4 is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: take in a character
+ *
+ * Return: 1 if c is letter, lowercase or uppercase otherwise 0
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return
+	else
+	return (0);
+}
