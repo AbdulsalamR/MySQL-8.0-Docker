db908a993b63a29ce664b63d699997868b03a92f
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2023-12-17 04:33:53 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,6 @@
 #include "search_algos.h"
+#include <stdio.h>
+
 /**
  * linear_search - search lnear form in array for element
  * @array: array list
@@ -8,17 +10,33 @@
  * Return: index where user looks for
  */
 
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i = 0;
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
+    
+    for (size_t i = 0; i < size; i++) {
+        printf("Comparing %d with %d\n", array[i], value);
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    
+    return -1;
+}
 
-	if (array == NULL)
-		return (-1);
-
-	for (; i < size; i++)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	} return (-1);
+int main() {
+    int array[] = {10, 1, 42, 3, 4, 42, 6, 7, -1, 9};
+    int size = sizeof(array) / sizeof(array[0]);
+    int value = 42;
+    
+    int index = linear_search(array, size, value);
+    
+    if (index != -1) {
+        printf("Value %d found at index %d\n", value, index);
+    } else {
+        printf("Value %d not found\n", value);
+    }
+    
+    return 0;
 }
